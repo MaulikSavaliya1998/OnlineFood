893381d8f9edff5f8214f2936725d6686e233bda
Maulik <savaliyamaulik98@gmail.com>
2020-02-11 22:54:15 +0530

    Creating Registration Form

--- FoodDelivery.Data/Repository/CustomerRepository.cs
+++ FoodDelivery.Data/Repository/CustomerRepository.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FoodDelivery.Data
+namespace FoodDelivery.Data.Repository
 {
-    public class Class1
+    class CustomerRepository
     {
     }
 }
---
+++ FoodDelivery.Web/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FoodDelivery.Data.Models;
+using FoodDelivery.Data.Repository;
+
+
+namespace FoodDelivery.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registration(Models.CustomerInfo customerInfo)
+        {
+            return View();
+        }
+    }
+}
