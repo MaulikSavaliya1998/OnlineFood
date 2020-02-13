933762d20efb377b103fe160e9b230dfcc5e68eb
Maulik <savaliyamaulik98@gmail.com>
2020-02-13 22:37:22 +0530

    final commit

--- FoodDelivery.Web/Controllers/HomeController.cs
+++ FoodDelivery.Web/Controllers/HomeController.cs
@@ -11,18 +11,6 @@ namespace FoodDelivery.Web.Controllers
         public ActionResult Index()
         {
             return View();
-
-
-<<<<<<< HEAD
-=======
-
-
-
-
-
-
-
->>>>>>> 7ef044e66c725c789a5b09ab68a4b3d33f3404eb
         }
     }
 }
